3e34283ea8a461859996bcfe7162f592ecb26687
Katie So <katieso86@gmail.com>
2021-09-29 17:05:41 -0700

    removed svg, and coded home view

--- js/scripts.js
+++ js/scripts.js
@@ -1,144 +1,144 @@
-const appendKeywords = (keywords) => {
-  return keywords.map((el) => (`<span class="candidate-employment-keywords">${el}</span>`)).join('');
-}
 
-$(document).ready(function(){
-
-  let cards = document.querySelector('.candidate-container')
-  let btnContainer = document.querySelector('.candidate-container__btn')
-  let bkBtn = document.querySelector('.back-icon')
-  let acceptedCandidates = []
-  let rejectedCandidates = []
-
-  const candidates = [
+const candidates = [
+  {
+    name: 'Ellie Jo',
+    currentTitle: 'Lead Developer',
+    currentEmployer: 'Google',
+    location: 'San Francisco',
+    yearsExperience: '10+',
+    school: 'UCSF',
+    degree: 'PHD',
+    subject: 'Computer Science',
+    workedFor: ['Google', 'Facebook'],
+    employmentHistory: [{
+      title: 'Lead Developer',
+      employer: 'Google',
+      dates: 'Jun 2013 to Present',
+      skills: ["C++", "CSS", "Java", "react", "Design System", "UI", "Prototype"]
+    },
     {
-      name: 'Eli Jo',
-      currentTitle: 'Developer',
-      currentEmployer: 'Google',
-      location: 'San Francisco',
-      yearsExperience: '10+',
-      school: 'UCSF',
-      degree: 'PHD',
-      subject: 'Computer Science',
-      workedFor: ['Facebook', 'Linkedin'],
-      employmentHistory: [{
-        title: 'Developer',
-        employer: 'Google',
-        dates: 'Jun 2013 to Present',
-        keywords: ["C++", "CSS", "Java", "react", "Design System", "UI", "Prototype"]
-      },
-      {
-        title: 'Cook',
-        employer: 'Facebook',
-        dates: 'Jun 2010 to Jun 2013',
-        keywords: ["HTML", "CSS", "UX"]
-      },
-      {
-        title: 'Jr Deveopler',
-        employer: 'Linkedin',
-        dates: 'Jun 2007 to Jun 2010',
-        keywords: ["Javascript", "CSS", "Jquery"]
-      }]
-    }, 
+      title: 'Software Architect',
+      employer: 'Facebook',
+      dates: 'Jun 2010 to Jun 2013',
+      skills: ["HTML", "CSS", "UX"]
+    },
     {
-      name: 'Ray Johnson',
-      currentTitle: 'Web Developer',
-      currentEmployer: 'Adobe',
-      location: 'San Francisco',
-      yearsExperience: '8',
-      school: 'Stanford University',
-      degree: 'Master',
-      subject: 'Computer Engineering',
-      workedFor: ['Facebook', 'Linkedin'],
-      employmentHistory: [{
-        title: 'Developer',
-        employer: 'Google',
-        dates: 'Jun 2013 to Present',
-        keywords: ["Java", "Docker", "React"]
-      },
-      {
-        title: 'Volunteer',
-        employer: 'Facebook',
-        dates: 'Jun 2010 to Jun 2013',
-        keywords: ["Javascript", "CSS", "Jquery"]
-      },
-      {
-        title: 'Jr Deveopler',
-        employer: 'Linkedin',
-        dates: 'Jun 2007 to Jun 2010',
-        keywords: ["Javascript", "CSS", "Jquery"]
-      }]
-    }, 
+      title: 'Jr Deveopler',
+      employer: 'Linkedin',
+      dates: 'Jun 2007 to Jun 2010',
+      skills: ["Javascript", "CSS", "Jquery"]
+    }]
+  }, 
+  {
+    name: 'Ray Johnson',
+    currentTitle: 'Web Developer',
+    currentEmployer: 'Adobe',
+    location: 'San Francisco',
+    yearsExperience: '8',
+    school: 'Stanford University',
+    degree: 'Master',
+    subject: 'Computer Engineering',
+    workedFor: ['Adobe', 'Facebook'],
+    employmentHistory: [{
+      title: 'Web Developer',
+      employer: 'Adobe',
+      dates: 'Jun 2013 to Present',
+      skills: ["Java", "Docker", "React"]
+    },
     {
-      name: 'Will Smith',
-      currentTitle: 'Developer',
-      currentEmployer: 'Google',
-      location: 'San Francisco',
-      yearsExperience: '10+',
-      school: 'UCSF',
-      subject: 'Computer Science',
-      degree: 'Bachelors',
-      workedFor: ['Microsoft', 'Linkedin'],
-      employmentHistory: [{
-        title: 'Janitor',
-        employer: 'Microsoft',
-        dates: 'Jun 2013 to Present',
-        keywords: ["Javascript", "CSS", "Jquery", "React", "Sass", "Angular"]
-      },
-      {
-        title: 'Software Engineer',
-        employer: 'Facebook',
-        dates: 'Jun 2010 to Jun 2013',
-        keywords: ["Javascript", "CSS", "Jquery"]
-      },
-      {
-        title: 'Developer',
-        employer: 'Microsoft',
-        dates: 'Jun 2010 to Feb 2010',
-        keywords: ["Javascript", "CSS", "Jquery"]
-      },
-      {
-        title: 'Intern',
-        employer: 'Twitter',
-        dates: 'Jun 2007 to Feb 2010',
-        keywords: ["Docker", "Java", "Jquery"]
-      }]
-    }
-  ];
+      title: 'UX Engineer',
+      employer: 'Facebook',
+      dates: 'Jun 2010 to Jun 2013',
+      skills: ["Javascript", "CSS", "Jquery"]
+    },
+    {
+      title: 'Jr Deveopler',
+      employer: 'Linkedin',
+      dates: 'Jun 2007 to Jun 2010',
+      skills: ["Javascript", "CSS", "Jquery"]
+    }]
+  }, 
+  {
+    name: 'Bill Smith',
+    currentTitle: 'Content Creator',
+    currentEmployer: 'Youtube',
+    location: 'Los Angeles',
+    yearsExperience: '10+',
+    school: 'University of California - LA',
+    subject: 'Threatre',
+    degree: 'Bachelors',
+    workedFor: [ 'Youtube', 'Apple'],
+    employmentHistory: [{
+      title: 'Content Creator',
+      employer: 'Youtube',
+      dates: 'Jun 2020 to Present',
+      skills: []
+    },
+    {
+      title: 'Receptionist',
+      employer: 'Apple',
+      dates: 'Jun 1994 to Feb 1995',
+      skills: []
+    }]
+  }, 
+  {
+    name: 'Scarlet Jo',
+    currentTitle: 'Frontend Engineer',
+    currentEmployer: 'Cypress',
+    location: 'San Francisco',
+    yearsExperience: '8',
+    school: 'Waterloo University',
+    degree: 'Bachelor',
+    subject: 'Computer Engineering',
+    workedFor: ['Cypress', 'Apple'],
+    employmentHistory: [{
+      title: 'Frontend Engineer',
+      employer: 'Cypress',
+      dates: 'Jun 2013 to Present',
+      skills: ["Java", "Docker", "React"]
+    },
+    {
+      title: 'UX Engineer',
+      employer: 'Apple',
+      dates: 'Jun 2010 to Jun 2013',
+      skills: ["Javascript", "CSS", "Jquery"]
+    },
+    {
+      title: 'Product Designer',
+      employer: 'Linkedin',
+      dates: 'Jun 2007 to Jun 2010',
+      skills: ["Javascript", "CSS", "Jquery"]
+    }]
+  }, 
+];
   
-  appendCard()
-
-
-  // $('.user-menu').hide('')
-
-  function appendCard(index = 0){
+const appendCard = (index = 0) => {
+  $('#nav-btn').attr({'src':"assets/backicon.png", "name":"backIcon"})
     if(candidates.length === index) {
-      // When no candidates available
       $('.candidate-container__btn').addClass('back-icon-fade')
       $('.candidate-container').append(`<div class="candidate-container__card"></div>`)
       $('.candidate-container__card').append(`
         <section class="complete-container">
           <img src="assets/confetti.png">
-          <p class="complete-container-text">You've completed all candidate review for this campaign. </p>
-          <p class="complete-container-text">TopFunnel will take it from here</p>
+          <p class="complete-container-text">You've completed all candidate reviews for this campaign. </p>
+          <p class="complete-container-text">TopFunnel will take it from here!</p>
           <button role="button" class="bk-btn">Back to Campaigns</button>
         </section>
       `)
     } else {
       const candidate = candidates[index];
       const getHistory = appendDetails(candidate.employmentHistory)
-      //figure this out
       $(`.candidate-container`).append(`
-      <div class="candidate-container__card" id="${index}">
-        <header class="candidate-container__header ">
+        <div class="candidate-container__card" id="${index}">
+          <header class="candidate-container__header">
             <p class="candidate-header-text candidate-header-name"> ${candidate.name} </p>
             <p class="candidate-header-text candidate-header-position"> ${candidate.currentTitle} at ${candidate.currentEmployer}</p>
             <p class="candidate-header-text candidate-header-summary"> <img class="-header-img-locationmarker" src="assets/locationmarker.png"> ${candidate.location}</p>
             <p class="candidate-header-text candidate-header-summary">Years of Experience <span class="candidate-header-summary-hilight">${candidate.yearsExperience} </span></p> 
             <p class="candidate-header-text candidate-header-summary">Attended <span class="candidate-header-summary-hilight">${candidate.school} </span></p> 
             <p class="candidate-header-text candidate-header-summary">${candidate.degree} in <span class="candidate-header-summary-hilight">${candidate.subject} </span></p> 
-            <p class="candidate-header-text candidate-header-summary">Worked at <span class="candidate-header-summary-hilight">${candidate.employmentHistory[0].employer}</span>&nbsp;and
-            <span class="candidate-header-summary-hilight">${candidate.employmentHistory[1].employer} </span></p> 
+            <p class="candidate-header-text candidate-header-summary">Worked at <span class="candidate-header-summary-hilight">${candidate.workedFor[0]}</span>&nbsp;and
+            <span class="candidate-header-summary-hilight">${candidate.workedFor[1]} </span></p> 
             <div class="candidate-header-socialmedia">
               <a href="https://github.com/ktso11"><img class="candidate-header-socialmedia-gitHub" alt="github thumbnail" src="assets/git.png"></a>
               <a href="https://www.linkedin.com/in/katie-so/"><img class="candidate-header-socialmedia-linkedin"  alt="linkedin thumbnail" src="assets/linkedin.png"></a>
@@ -157,22 +157,17 @@ $(document).ready(function(){
           </footer>
         </div>
       `)
+
     }
   }
 
-//seperation of concern 
-//string interpolation 
-//change keyword to skills
-//change variables to jquery everywhere 125... and more 
-//use variable caching
-//change fucntion to es6 - they will ask why you have es5 and 6 everywhere 
-//move functions that do not acess the DOM to before document ready <--google why
-//- - they might ask why I am hiding UI instead of using pages
-
-
-  function appendDetails(employment){
+  const appendSkills = (skills) => {
+    return skills.map((el) => (`<span class="candidate-employment-skills">${el}</span>`)).join('');
+  }
+  
+  const appendDetails = (employment) => {
     return  employment.map((el) => {
-      const keywords = appendKeywords(el.keywords)
+      const skills = appendSkills(el.skills)
       return (
         `<main class="candidate-container__employment">
             <section class="candidate-employment">
@@ -183,8 +178,8 @@ $(document).ready(function(){
                 <p class="candidate-employment-position"> ${el.title}</p>
                 <p class="candidate-employment-summary"> ${el.employer}</p>
                 <p class="candidate-employment-summary"> ${el.dates}</p>
-                <div class="candidate-employment__keywords-container">
-                  ${keywords}
+                <div class="candidate-employment__skills-container">
+                  ${skills}
                   <img class="candidate-employment-line" src="assets/line2.png">
                 </div>          
               </div>
@@ -194,8 +189,13 @@ $(document).ready(function(){
     })
   }
 
+$(document).ready(function(){
 
-
+  let cards = $('.candidate-container');
+  let resultButton = $('.candidate-container__btn')
+  let acceptedCandidates = []
+  let rejectedCandidates = [] 
+  appendCard()
 
 //Animations/Interactions
   $('.screen').on('scroll', function(){
@@ -206,104 +206,133 @@ $(document).ready(function(){
     }
   })
 
-  let click = 0 
-  //count to ensure all candidates are reviewed, and prevent repeats
-  // when check/cross/pass buttons are clicked, click count incredments
-  // assign zindex to current card to ensure new card appended is at the button
-  // apply css to slide card to review new card
-  // remove old card completely from UI 
-
-  function intEffect(move, currentIndex){
-    click = click +1
-    $('#'+currentIndex).css('z-index',"5")
-    appendCard(click)
-    $('#'+currentIndex).addClass('candidate-card__slide'+move)
-    setTimeout(function() {
-      $('#'+currentIndex).remove()
-    }, 700);
-  }
-
   //Decison Logic (based on user decision to Accept, Reject or Skip)  ***
-  btnContainer.addEventListener('click', function(event) {
-    //find the index of the card in display
-    let getIndex = document.getElementsByClassName('candidate-container__card')
-    let index = getIndex[getIndex.length-1].id //because I have a timer 
-    console.log(index)
+  resultButton.on('click', function(event) {
+    let getIndex = $('.candidate-container__card')
+    let index = getIndex[getIndex.length-1].id
     if(click === candidates.length) {
-      //When out of candidates, remove buttons 
       $('.candidate-container__btn').hide()
     }
     if(click < candidates.length && event.target.id === "click-yes"){
+      console.log("clicked")
       acceptedCandidates.push(candidates[index].name)
-      intEffect("right", index)
+      animateSwipe("right", index)
     }  
     if(click < candidates.length && event.target.id === "click-no"){
       rejectedCandidates.push(candidates[index].name)
-      intEffect("left",index)
+      animateSwipe("left",index)
     } 
     if(click < candidates.length && event.target.id === "click-skip"){
-      //move to end of arr 
       candidates.push(candidates[index]);
-      intEffect("left",index)
-      // $('.candidate-container__btn').removeClass('back-icon-fade') //to avoid buttons hide when counter reached
+      animateSwipe("left",index)
     } 
   });
 
-  cards.addEventListener('click', function(event){
-    if (event.target.className === "campaign-btn"){
-      closeHome()
-    } if (event.target.className === "bk-btn") {
-      openHome()
-      $('.candidate-container__card').append(`
-      <section class="home-container">
-        <img class="tf-logo" src="assets/tf.svg">
-        <img class="notification-svg" src="assets/noti-done.svg">
-        <img role="button" class="campaign-btn__unactive" src="assets/reviewed.svg">
-      </section> 
-    `)
-    }
-  })
+  let click = 0 
+  const animateSwipe = (...args) => {
+    click = click +1
+    $('#'+args[1]).css('z-index',"5")
+    appendCard(click)
+    $('#'+args[1]).addClass('candidate-card__slide'+args[0])
+    setTimeout(() => {
+      $('#'+args[1]).remove()
+      $('.screen').scrollTop(0)
+    }, 580);
+  }
 
-  let navToggle = false
-  bkBtn.addEventListener('click', function(event){
-    if(navToggle === true ){
-      closeHome()
-    } else {
-      openHome()
-      $('.candidate-container__card').append(`
-      <section class="home-container">
-        <img class="tf-logo" src="assets/tf.svg">
-        <img class="notification-svg" src="assets/noti.svg">
-        <img class="campaign-btn" src="assets/campaign.svg">
-      </section> 
+  cards.on('click', function(event){
+    //Click on Campaign
+    if (event.target.className === "campaign-btn"){
+      appendCard(0)
+      $('.candidate-container__btn').removeClass('back-icon-fade')
+      click = 0 
+    } 
+    //Back to Campaign
+    if (event.target.className === "bk-btn") {
+      backtoDash()
+      // <img class="notification-svg" src="assets/noti-done.svg">
+
+
+      $('.candidate-container').append(`
+        <section class="candidate-container__card">
+          <div class="home-container">
+            <img class="tf-logo" src="assets/tf.svg">
+            <div class="notification-block">
+              <img class="notification-hand" src="assets/confetti.png">
+              <div class="notification-text-container">
+                <span class="notification-greeting">You are all caught up!</span>
+                <p class="notification-msg">Your candidates status will be available soon</p>
+              </div>
+            </div>
+            <section class="campaign-list">
+              <div class="campaign-list-col"></div>
+              <div class="campagin-list-name">Frontend Engineer <p class="campagin-list-reviewdate">Last reviewed Sept 30</p></div>
+              <div class="campaign-list-count">`+click+` <br>Reviewed</div> 
+            </section>           
+          </div> 
+        </section>
       `)
     }
   })
 
-  function closeHome(){
-    document.getElementById('nav-btn').src="assets/backicon.png"
-    navToggle = 0
-    console.log("clicked on hamburger")
-    // $('.user-menu').addClass('menu__slideright')
-    $('.candidate-container__card').remove()
-    $('.candidate-container__btn').removeClass('back-icon-fade')
-    appendCard(0)
-    click = 0 
-  }
+  $('.back-icon').on('click', function(event){
+    let icon = event.target.name
+    if(icon === "userIcon" ){
+      $('#nav-btn').attr({'src':"assets/backicon.png", "name":"backIcon"})
+      $('.black-screen').css('height','812')
+      $('.black-screen').toggleClass('black-screen-active')
+      $('.user-menu').addClass('menu__slideright')
+      $('.back-icon').hide()
+      $('.candidate-container__btn').addClass('back-icon-fade')
+    } if(icon === "backIcon" ) {
+      backtoDash()
+      let reviewCount = candidates.length - click
+      $('.candidate-container').append(`
+        <section class="candidate-container__card">
+          <div class="home-container">
+            <img class="tf-logo" src="assets/tf.svg">
+            <div class="notification-block">
+              <img class="notification-hand" src="assets/hand.png">
+              <div class="notification-text-container">
+                <span class="notification-greeting">Hello Joe!</span>
+                <p class="notification-msg">You have <b>`+ reviewCount + ` candidates</b> to review</p>
+              </div>
+            </div>
+            <section class="campaign-btn campaign-list">
+              <div class="campaign-list-col2"></div>
+              <div class="campagin-list-name2">Frontend Engineer </div>
+              <div class="campaign-list-count2">
+                `+ reviewCount +`
+                <img class="countIcon"src="assets/counticon.png">
+              </div> 
+            </section>   
+           
+          </div>
+        </section> 
+      `)
+    } 
+  })
 
-  function openHome(){
-    document.getElementById('nav-btn').src="assets/hamburger.png"
-    navToggle = true
-    // $('.user-menu').removeClass('menu__slideright', 500)
+  $('.black-screen').on('click', function(event){
+    $('#nav-btn').attr({'src':"assets/avatar.svg", "name":"userIcon"})
+    $('.user-menu').removeClass('menu__slideright')
+    $('.black-screen').toggleClass('black-screen-active')
+    setTimeout(() => {
+      $('.back-icon').show()
+      $('.black-screen').css('height','0')
+    }, 500)
+  })
+
+  const backtoDash = () => {
     $('.candidate-container__btn').addClass('back-icon-fade')
     $('.candidate-container__card').remove()
-    $('.candidate-container').append(`<div class="candidate-container__card"></div>`)
+    $('#nav-btn').attr({'src':"assets/avatar.svg", "name":"userIcon"})
   }
 
-  // Show User's clicks throughout session 
-  cards.addEventListener('click', function(event){
+  cards.on('click', function(event){
     $('.results').append(`User clicked on "${event.target.className}". <br>`)
-    console.log(acceptedCandidates)
+    console.log("accepted: "+acceptedCandidates)
+    console.log("Rejected: "+rejectedCandidates)
   })
 
 
